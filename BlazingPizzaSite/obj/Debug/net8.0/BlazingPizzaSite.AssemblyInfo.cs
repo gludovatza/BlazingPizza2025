f303f3e3f8118c3a47f941db218ff5ad215acb6e
Gludovátz Attila <attila@gludovatz.hu>
2025-02-17 13:21:21 +0100

    Csomagok hozzáadása az adatbázis-hozzáférés támogatásához

--- BlazingPizzaSite/obj/Debug/net8.0/BlazingPizzaSite.AssemblyInfo.cs
+++ BlazingPizzaSite/obj/Debug/net8.0/BlazingPizzaSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b21171499055ca06e7038ca81e77fc56c035f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
